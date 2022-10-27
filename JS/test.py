39672377680b93750d8b4905645aaf3208c8442f
test <test@test.com>
2022-10-27 13:39:56 -0700

    u

--- JS/test.py
+++
@@ -1,9 +0,0 @@
-#!/hds/eda/virtualenv/3.7.4/venv/bin/python
-
-import chardet
-import os
-
-for root, dirs, names in os.walk('.'):
-    print root
-    for n in names:
-    print '%s => %s (%s)' % (n, chardet.detect(n)['encoding'], chardet.detect(n)['confidence'])
